5a788007439520493cc056f2f52e26e0340b41d7
Newer <newerxd1@gmail.com>
2023-03-07 21:06:19 +0530

    Added Navbar Animation during scroll.

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,3 +54,15 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+      document.querySelector(".nav").style.opacity = "1";
+    document.querySelector(".nav").classList.remove("hide");
+  } else {
+    document.querySelector(".nav").classList.add("hide");
+  }
+  prevScrollpos = currentScrollPos;
+}
