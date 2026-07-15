9468425b7ab341a88eba85a5a64bc2c5a2f280e1
Raunak Singh <116626403+Newer1107@users.noreply.github.com>
2026-07-15 13:27:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: '::',
     port: 8080,
@@ -11,10 +10,18 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+
+  preview: {
+    host: '::',
+    port: 8080,
+    allowedHosts: ['raunaktech.site'],
+  },
+
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-}));
+});
